4209a7f0e081affb426d0729a2578f340e26a03b
vedija <vedija.jagtap99@gmail.com>
2020-02-26 20:44:28 -0500

    ClassroomProject03 changes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const axios = require('axios');
 const cors = require('cors')
 const app = express()
 const port = 3000
 
 app.use(cors())
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+    const info = axios.get('https://r3n249devl.execute-api.us-east-2.amazonaws.com/prod')
+    .then((response) => {
+        res.send(response.data);
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
